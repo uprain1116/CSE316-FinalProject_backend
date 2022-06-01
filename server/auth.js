fbd4a3117ebd98d6995f181664a193bab7d20018
uprain1116 <74394318+uprain1116@users.noreply.github.com>
2022-06-02 04:36:46 +0900

    Update auth.js

--- server/auth.js
+++ server/auth.js
@@ -1,19 +1,19 @@
-// const Note = require('./models/note');
-// const {wrapAsync} = require('./helper');
-//
-// module.exports.isLoggedIn = (req, res, next) => {
-//     if (!req.session.userId) {
-//         throw new Error("Need to login first");
-//     }
-//     next();
-// }
-// //
-// // // If the author has an agent, the logged in user must be that agent to access
-// // module.exports.isAgent = wrapAsync(async (req, res, next) => {
-// //     const id = req.params.id;
-// //     const note = await Note.findById(id);
-// //     if (note.agent && !note.agent.equals(req.session.userId)) {
-// //         throw new Error("Not an authorized agent for this author");
-// //     }
-// //     next();
-// // });
+const User = require('./models/user');
+const {wrapAsync} = require('./helper');
+
+module.exports.isLoggedIn = (req, res, next) => {
+    if (!req.session.userId) {
+        throw new Error("Need to login first");
+    }
+    next();
+}
+
+// If the author has an agent, the logged in user must be that agent to access
+module.exports.isAgent = wrapAsync(async (req, res, next) => {
+    const id = req.params.id;
+    const user = await User.findById(id);
+    if (user.agent && !user.agent.equals(req.session.userId)) {
+        throw new Error("Not an authorized agent for this author");
+    }
+    next();
+});
